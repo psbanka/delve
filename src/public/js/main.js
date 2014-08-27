18e310d347b0caacf3d6576acc80dfea1329852d
Peter Banka <peter.banka@cyaninc.com>
2014-08-26 23:17:18 -0700

    Working website with bootstrap

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
-// define(function (require) {
-define(function () {
+define(function (require) {
 
-    //var $ = require('jquery');
+    var $ = require('jquery');
+    var cardPageTmpl = require('delve-client/tmpl/cards.tmpl');
 
     var DelveClient = function() {};
 
@@ -13,6 +13,8 @@ define(function () {
         */
         main: function() {
             console.log('delveClient is online.');
+            var cardPageHtml = cardPageTmpl({});
+            $('body').append(cardPageHtml);
         },
     };
 
